1fede40a1a39a5f0b8d98d9d919bbf2fd28cf2de
ivymelanin <leratokhauta20@gmail.com>
2024-10-10 16:22:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
     let instructionInput = document.querySelector("#instructions");
     let apiKey ="77e54fc3f3o315bb21050ac08t45b6af";
     let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
-    let context = "Write any poem in two paragraphs and be short and clear. start every new sentence in a new line ";
+    let context = "Write any poem that in two paragraphs, be short and clear. generate a poem in basic HTML and separate each line with a < b /> ";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let poemElement = document.querySelector("#poem");
